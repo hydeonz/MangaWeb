cc2eb9db5f264a1fbc9fd82d73b8398891c00ef3
Yuriy <yurakirin9545@gmail.com>
2024-05-15 19:22:46 +0300

    Init project, migrations, models

---
+++ database/migrations/2024_05_15_154942_create_mangas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mangas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id');
+            $table->string('title')->nullable(false);
+            $table->string('description')->nullable(false);
+            $table->date('release_date')->nullable(false);
+            $table->string('image_path')->nullable(false);
+            $table->boolean('is_deleted')->nullable(false)->default(false);
+            $table->foreign('author_id')->references('id')->on('authors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mangas');
+    }
+};
---
+++ database/migrations/2024_05_15_160559_create_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('manga_id')->unique();
+            $table->integer('rating')->nullable(false);
+            $table->string('review')->nullable(true);
+            $table->foreign('manga_id')->references('id')->on('mangas');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
